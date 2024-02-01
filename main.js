5e4a94afcaa7198d31496baa3a00d62e2e06a583
ameena117 <ameenajadallah@gmail.com>
2024-02-01 23:52:30 +0200

    add local Storage

--- main.js
+++ main.js
@@ -4,12 +4,32 @@ console.log(item);
 let list = document.querySelector('.list');
 console.log(list);
 const form = document.querySelector("form");
-//=====================
+let todos = localStorage.getItem("todos");;
+console.log(todos);
+arrayOfObjects.push(todos);
+console.log(arrayOfObjects);
+if (arrayOfObjects[0] == null) {
+    arrayOfObjects = [];
+}
+
+else {
+    console.log(arrayOfObjects);
+    arrayOfObjects = arrayOfObjects[0].split(",");
+    console.log(arrayOfObjects);
+    list.innerHTML = arrayOfObjects.map(function (e) {
+        return `
+        <li>
+            <input type="checkbox" name="item" class="checkbox" >
+            <span class="item">${e}</span>
+        </li>
+`;
+    }).join(' ');
+}
 
 form.onsubmit = function Add(e) {
     e.preventDefault();
     arrayOfObjects.push(item.value);
-    
+
     list.innerHTML = arrayOfObjects.map(function (e) {
         return `
             <li>
@@ -19,12 +39,13 @@ form.onsubmit = function Add(e) {
     `;
     }).join(' ');
 
+    //to add in localStorage
+    localStorage.setItem("todos", arrayOfObjects);
     item.value = '';
     let checkbox = document.querySelectorAll("li .checkbox");
     for (let i = 0; i < checkbox.length; i++) {
-        checkbox[i].addEventListener("change", function(e) {
+        checkbox[i].addEventListener("change", function (e) {
             e.target.nextElementSibling.classList.toggle("checked")
         });
-        
     }
 };
